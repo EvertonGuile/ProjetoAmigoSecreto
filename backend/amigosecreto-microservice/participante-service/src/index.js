a72db5970e4195c3298880091d9a0c494614ff35
EvertonGuile <evertonguile@hotmail.com>
2024-02-15 00:32:20 -0300

    versão 1 da API participante - completa

--- backend/amigosecreto-microservice/participante-service/src/index.js
+++ backend/amigosecreto-microservice/participante-service/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+const routes = require('./routes/routes');
+
 const app = express();
 
 app.use(express.json());
 
+/*
 // MÉTODO GET
 app.get('/:nome', (request, response) => {
     return response.send("Hello World! Seja bem vindo " + `${request.params.nome}`);
@@ -14,6 +19,14 @@ app.post('/participante', (request, response) => {
     console.log(request.body);
     const { nome } = request.body;
     return response.json({"mensagem":`Olá Mundo ${nome}`});
+});
+*/
+
+app.use(routes);
+
+mongoose.connect('mongodb+srv://evertinhuplayer:O93fSpTIvhf92qxL@firstcluster.vwo5uub.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 app.listen(3333);
